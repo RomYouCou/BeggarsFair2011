f6acf1c5440d095b7a57e596efb511255656ad3c
Ben Evans <ben@bensbit.co.uk>
2013-01-20 17:00:18 +0000

    Git Import/Initial Commit

---
+++ index.php
@@ -0,0 +1,253 @@
+<?php
+//Added Failsafe for inDev
+if (file_exists('../wp-blog-header.php')) {
+define('WP_USE_THEMES', false);
+require('../wp-blog-header.php');
+
+}
+ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+  <head>
+    <title>RYC: Beggars Fair Youth Stage 2011</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <META NAME="Description" CONTENT="An Open Gig in the heart of Romsey. Featuring: Thinking for Tuesdays, Kate Mac, The Mascots, The 3rd League, The Harlequin, Ash & Quantz, Hot Persuit, Flintlock."/>
+    <meta property="og:title" content="RYC: Beggars Fair Youth Stage 2011"/>
+    <meta property="og:activity" content="band"/>
+    <meta property="og:site_name" content="Romsey Youth Council Website"/>
+    <meta property="og:description" content="Featuring: Thinking for Tuesdays, Kate Mac, The Mascots, The 3rd League, The Harlequin, Ash & Quantz, Hot Persuit, Flintlock."/>
+    <meta property="og:image" content="http://www.ryc.org.uk/wp-content/gallery/beggars-fair-youth-stage-2010/IMG_1224.JPG"/>
+    <meta property="fb:admins" content="1089532097"/>
+    <link rel="stylesheet" href="blueprint/screen.css" type="text/css" media="screen, projection"/>
+    <link rel="stylesheet" href="blueprint/print.css" type="text/css" media="print"/>    
+    <!--[if lt IE 8]><link rel="stylesheet" href="blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+    <link  href="http://fonts.googleapis.com/css?family=Special+Elite:regular" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+    body {
+      background-color:#333;
+      background: url(1.JPG) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+      filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='.myBackground.jpg', sizingMethod='scale');
+      -ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='myBackground.jpg', sizingMethod='scale')";
+      text-align:left;
+      }
+    h1 {
+      margin:0;
+      }
+    
+    #after {
+      background-color:#666;
+      background-image:url('bg.jpg');
+      -webkit-box-shadow: 0px 0px 10px #000;
+      -moz-box-shadow: 0px 0px 10px #000;
+      box-shadow: 0px 0px 10px #000; 
+      color:#fff;
+      padding-bottom:2px;
+      padding-top:5px;
+      margin-bottom:5px;
+      }
+    #content {
+      background-color:#666;
+      background-image:url('bg.jpg');
+      -webkit-box-shadow: 0px 0px 10px #000;
+      -moz-box-shadow: 0px 0px 10px #000;
+      box-shadow: 0px 0px 10px #000; 
+      color:#fff;
+      padding-bottom:2px;
+      padding-top:5px;
+      }
+    #RYC {
+      font-size:100px;
+      }
+    .allheadings {
+      margin:0;
+      font-family: 'Special Elite', serif;
+      color:#fff;
+      text-shadow: 0px 0px 5px #000000;
+      filter: Shadow(Color=#666666, Direction=135, Strength=5);
+      
+
+      }
+    .content .allheadings, .content .smallheadings {background-image:url('bg.jpg');}
+    .smallheading {
+      font-size:33px;
+      line-height: 1.2;
+      }
+    #maincontainer {
+      margin-top:40px;
+      font-size:120%;
+      
+      }
+    #mainsitebar{
+      width:100%;
+      background-color:#000;
+      height:20px;
+      opacity:0.6;
+      position:fixed;
+      top:0;
+      }
+    #mainsitebar:hover {
+      opacity:1;
+      width:100%;
+      }
+    #mainsitebar a {
+      text-decoration:none;
+      padding:5px;
+      color:#fff;
+      }
+    .differentheading {
+      color:#009900;
+      }
+    a {
+      text-decoration:none;
+      color:#0b99bd;
+      }
+    #footer {
+      background-color:#666;
+      background-image:url('bg.jpg');
+      -webkit-box-shadow: 0px 0px 10px #000;
+      -moz-box-shadow: 0px 0px 10px #000;
+      box-shadow: 0px 0px 10px #000; 
+      color:#fff;
+      padding-bottom:2px;
+      padding-top:2px;
+      margin-top:10px;
+      margin-bottom:10px;
+      }
+    #Ben {
+      opacity:0.5;
+      }
+    #Ben:hover {
+      opacity:0.7;
+      }
+    .round {
+      -moz-border-radius: 10px;
+      border-radius: 10px;
+      }
+    .insetlight {
+      -webkit-box-shadow: inset 0px 0px 10px #121212;
+-moz-box-shadow: inset 0px 0px 10px #121212;
+box-shadow: inset 0px 0px 10px #121212; 
+      padding-top:20px;
+      margin-bottom:10px;
+      }
+    .time, .bandname {
+      font-family: 'Special Elite', serif;
+      font-size:30px;
+      color:#fff;
+      margin-bottom:3px;
+      
+      }
+    .description p {margin:0;}
+    .bandbox {padding-bottom:10px;border-bottom:1px solid #000;margin-bottom:10px;opacity:0.7;}
+    .bandbox:hover {opacity:1;}
+    .bandbox:last-child {border:0;padding-bottom:0;}
+    </style>
+  </head>
+  <body>
+    <div id="mainsitebar">
+    
+      <a href="/">&larr; Click Here to go to the Main Romsey Youth Council Site</a>
+    </div>
+    <div id="maincontainer" class="container">
+      <div class="span-6">
+        <h1 id="RYC" class="allheadings">RYC:</h1>
+      </div>
+      <div class="span-17">
+        <h2 class="allheadings smallheading">Beggars Fair 2011 <span class="differentheading">Saturday 9th</span></h2>
+      </div>
+      <div class="span-17">
+        <h2 class="allheadings smallheading" >Youth Stage <span class="differentheading">July 12 - 6PM</span></h2>
+      </div>
+      <div class="clear"></div>
+      <div id="after" class="span-22 append-1 prepend-1 round">
+        <div style="border-bottom:1px solid #fff;margin:5px 0 5px 0;" class="span-22">
+          <h2 class="allheadings smallheading">2011 Done... 2012 to Come</h2>
+        </div>
+        <div class="clear"></div>
+        <div class="span-22">
+          <p>Wow. 2011 was amazing with Memorial Park pretty much full of people supporting Romsey's brilliant musical talent raising over 1.5 grand for next years event. However we also need people to join us and make next years event happen. So if your up for it email us (contact [at] ryc.org.uk)</p>
+        </div>
+      </div>
+      <div class="clear"></div>
+      <div id="content" class="span-22 append-1 prepend-1 round">
+        <div style="border-bottom:1px solid #fff;margin:5px 0 5px 0;" class="span-22">
+          <h2 class="allheadings smallheading">Line Up</h2>
+        </div>
+        <div class="clear"></div>
+        <div class="span-20 append-1 prepend-1 insetlight" style="background-color:black;padding-top:20px;padding-bottom:10px;background-image:url('bgdark.jpg');">
+        <?php 
+        
+        function BandTime($StartTime, $BandName, $BandDescription, $BandWebsite, $BandFacebook, $BandMySpace) {
+        echo '<div class="span-20 bandbox">
+            <div class="span-4"><h3 class="time">' . $StartTime . '</h3></div>
+            <div class="span-15"><h3 class="bandname">' . $BandName . '</h3></div>
+            <div class="prepend-4 span-16 description" ><p>' . $BandDescription . '</p>';
+            if($BandWebsite || $BandFacebook || $BandMySpace != ''){echo '<p>';} 
+            if($BandWebsite != '') {echo '<a href="' . $BandWebsite . '">Website</a> ';}
+            if($BandFacebook != '') {echo '<a href="' . $BandFacebook . '">Facebook</a> ';}
+            if($BandMySpace != '') {echo '<a href="' . $BandWebsite . '">MySpace</a> ';}
+            
+            
+            if($BandWebsite || $BandFacebook || $BandMySpace != ''){echo '</p>';}
+        echo '</div></div>';
+        } ?>
+<?php BandTime('12:00', 'Ash & Quantz', 'We have been together for nearly a year now and have already experience some awesome things such as performing at Portsmouth Guildhall and reaching the regional finals of the national competition, OpenMicUk. We were one round away from performing at the O2 Arena but unfortunately didn&#39;t make it :( </p><p>We now look to get more and more gigs so we can increase our experience in performing :)', '' , 'http://www.facebook.com/pages/AshQuantz-MusicUK/168385296554833', ''); ?>
+<?php BandTime('12:30', 'Forever Under Fire', 'Forever Under Fire are a local acoustic/indie duo consisting of cousins, George Dummett and Joe Mongan ', '' , 'http://www.facebook.com/pages/Forever-Under-Fire/177384598952938', 'http://www.myspace.com/566324439'); ?>
+<?php BandTime('13:00', 'Tom Crouch', 'Tom Crouch is a 17 year old acoustic singer songwriter, hailing from Southampton. His one goal is to write music which connects, unites and inspires people, and to show some love and compassion that this world somehow lacks. His music is powerful and passionate, with catchy melodies and foot-stomping beats.', '' , 'http://www.facebook.com/tomcrouchmusic/', 'http://www.myspace.com/tomcrouchmusic/'); ?>
+<?php BandTime('13:30', 'The Mascots', '', 'https://themascots.wordpress.com/' , 'http://www.facebook.com/themascots', ''); ?>
+<?php BandTime('14:00', 'Fallen Destiny', 'Fallen Destiny are a local band from Southampton, playing covers from some of the best rock bands. With members, Sammyy Rowles, Ollie Chandler, Billy Saville and Jordie Lee.', '' , 'https://www.facebook.com/pages/Fallen-Destiny/159692230765608', ''); ?>
+<?php BandTime('14:30', 'The 3rd League ', 'The 3rd League are a four piece indie/punk rock band based in Southampton that formed in mid-2009. Since then we have gigged all around the South playing busy headline shows. We have also supported some great acts such as ISLET and even Airship.', '' , 'http://www.facebook.com/the3rdleague', ''); ?>
+<?php BandTime('15:00', 'The Harlequin', 'The harlequin Formed in late 2010, 4-piece Indie/Rock band based in Southampton and Romsey.', '' , 'http://www.facebook.com/pages/The-Harlequin/199236073421480 ', 'http://www.myspace.com/search/myspace?q=the%20harlequin&ac=t'); ?>
+<?php BandTime('15:30', 'Thinking For Tuesday', 'We are a melodic rock band formed in November 2006 thanks to the wonders of the internet and a friendship formed around music and a love of guitars.  A combination of catchy vocal lines, impressive instrumentals and a vibrant live set has earned us a number of credible support slots in the past and a building reputation locally and beyond. We boast a repertoire of radio-friendly songs with an innovative edge thanks to our range of influences.', 'http://www.thinkingfortuesday.com' , '', ''); ?>         
+<?php BandTime('16:30', 'Flintlock', 'Blues, Rock, Surf just anything that sounds good :P', '' , '', ''); ?>
+<?php BandTime('17:00', 'Kate Mac', 'Kate Mac is an acoustic/indie/pop singersongwriter from Romsey whose music echoes influences of The Kooks Kate Nash The Arctic Monkeys etc.', '' , 'http://www.facebook.com/pages/Kate-MacDonald/163016710404768', 'http://www.myspace.com/katemacdonaldmusic'); ?>
+        </div>
+        <div style="border-bottom:1px solid #fff;margin:5px 0 5px 0;" class="span-22">
+          <h2 class="allheadings smallheading">Donate a Day</h2>
+        </div>
+        <div class="clear"></div>
+        <div class="span-22">
+        <p>If you would like to donate some of your time on the day to help with this event by doing things such as running stalls (face painting, luccky tip and the like). Please click <a href="http://www.ryc.org.uk/contact-us/">HERE</a> to register and get more information.</p>
+        </div>
+
+        <div style="border-bottom:1px solid #fff;margin:5px 0 5px 0;" class="span-22">
+          <h2 class="allheadings smallheading">Get Updates</h2>
+        </div>
+        <div class="clear"></div>
+        <div class="span-22">
+        <p>The best way to get updates on what is happening is to either follow us on <a href="http://twitter.com/RomYouCou">Twitter</a>, <a href="http://www.facebook.com/group.php?gid=47359950442">Facebook Group</a> or the <a href="http://www.facebook.com/event.php?eid=196717237013750">Facebook Event</a>. </p>
+        </div>
+      </div>
+      <div class="clear"></div>
+      <div id="footer" class="span-22 append-1 prepend-1 round">
+        &copy; RYC (Romsey Youth Council) <span id="Ben">- Hand Coded by <a href="http://www.facebook.com/bencevans">Ben Evans.</a></span>
+      </div>
+    </div>
+    <?php
+//Added Failsafe for inDev
+if (file_exists('../wp-blog-header.php')) {
+if ( is_user_logged_in() ) {
+    
+} else { ?>
+   <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15765254-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?php   
+} }
+?>
+  </body>
+</html>
